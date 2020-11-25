7740c535e2e1954a1196b30d9ae7e79039f61d64
ultrapre <magsec2@gmail.com>
2020-11-25 16:18:45 +0800

    run fail

--- src/core/StelFileMgr.cpp
+++ src/core/StelFileMgr.cpp
@@ -85,8 +85,11 @@ void StelFileMgr::init()
 		installDir = ".";
 	}
 	else
-	{
-	#if defined(Q_OS_MAC)
+    {
+    #if defined(Q_OS_ANDROID)
+        QFileInfo installLocation(QFile::decodeName("assets:"));
+        QFileInfo checkFile(QFile::decodeName("assets:" "/" CHECK_FILE));
+    #elif defined(Q_OS_MAC)
 		QString relativePath = "/../Resources";
 		if (QCoreApplication::applicationDirPath().contains("src")) {
 			relativePath = "/../..";
